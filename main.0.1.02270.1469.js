3af0b536c65f4cca38475f700301ba39f97941aa
swift <rajaram126@gmail.com>
2023-04-12 23:16:25 -0700

    Update main.0.1.02270.1469.js

--- main.0.1.02270.1469.js
+++ main.0.1.02270.1469.js
@@ -1,3 +1,3 @@
 document.addEventListener('DOMContentLoaded', function() {
-  alert('This is an alert box displayed as soon as the script is loaded.');
+ window.open('https://www.google.com', '_blank');
 });
