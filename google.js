91ba947718c6f1ce642526b9703a43159d9aeeca
swift <rajaram126@gmail.com>
2023-04-12 23:23:20 -0700

    Create google.js

---
+++ google.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  // Option 1: Open Google in a new tab/window
+  window.open('https://www.google.com', '_blank');
+  
+  // Option 2: Navigate to Google in the current tab/window (uncomment the line below to use this option)
+  // window.location.href = 'https://www.google.com';
+});
